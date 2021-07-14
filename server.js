b7149e28f10ca8a23cb7979aa2dd419554213cd9
Asher <asher.bergtraun@gmail.com>
2021-07-13 22:56:41 -0700

    set port and required variables

---
+++ server.js
@@ -0,0 +1,8 @@
+//call PORT and specify number
+const PORT = process.env.PORT || 3001;
+
+//require variables from package-lock.json
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
