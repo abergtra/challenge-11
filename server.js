37da0a79604e8da63bb962dd230e75ea215cba29
Asher <asher.bergtraun@gmail.com>
2021-07-14 22:13:35 -0700

    updated dependencies

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 //call PORT and specify number
 const PORT = process.env.PORT || 3001;
 
-//require variables from package-lock.json
+//Dependencies from package-lock.json
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
+const allNotes = require('./db/db.json')
+
+//Set up Express App
