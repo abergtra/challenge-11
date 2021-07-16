5eacd817af55ce17eff4660ac60d4b09a60345c6
Asher <asher.bergtraun@gmail.com>
2021-07-15 17:57:39 -0700

    added use to parse incoming data

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
+//Dependencies from database folder
 const allNotes = require('./db/db.json')
 
 //Set up Express App
+const app = express();
+
+//Parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+//Parse incoming JSON data
+app.use(express.json());
+//Serve static files from "public" directory
+app.use(express.static('public'));
+
